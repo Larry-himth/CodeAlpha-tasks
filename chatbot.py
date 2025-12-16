d1cf4f5c05b524c9603ec533e06b194b99adc077
Larry <hillarybahati@icloud.com>
2025-12-16 17:07:23 +0300

    added comments to my chatbot code

--- chatbot.py
+++ chatbot.py
@@ -1,16 +1,27 @@
+# startup message to show that the chatbot is running
 print("ChatBot is running, Type something\n")
 
+# boolean variable to control the loop
 running = True
 
 while(running):
+    # ask the user for input
     user_message = input("You: ").lower()
     
+    # check if the user greets the chatbot
     if user_message == "hello" or user_message == "hi" or user_message == "hey":
-        print("ChatBot: Hi!")
+        print("ChatBot: Hi!") 
+    
+    # check if the user asks how the chatbot is doing
     elif user_message == "how are you" or user_message == "how are you doing":
         print("ChatBot: I'm fine, thanks!")
+        
+    # check if the user ends the conversation
     elif user_message == "bye" or user_message == "stop":
         print("ChatBot: Goodbye!")
+        # stop the loop
         running = False
+        
+    # handle unrecognized input
     else:
         print("I dont understand!")
